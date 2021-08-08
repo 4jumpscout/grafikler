fc1d609252e78284b9531b3dd29ac699f1d27000
4jumpscout <86972257+4jumpscout@users.noreply.github.com>
2021-08-08 23:54:20 +0300

    Update pasta grafiği.py

--- pasta grafiği.py
+++ pasta grafiği.py
@@ -3,18 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.pyplot import figure
 kral=pd.read_json("goals.json")
-i=0
-j=0
-mac_basina_gol=[]
-while i<10:
-    mac_basina_gol.append((kral[j:j+1]["GOL"])/kral[j:j+1]["MAÇ SAYISI"])
-    i=i+1
-    j=j+1
-
 ölçek=[]
 label=[]
-i=0
-j=0
 
 plt.title("Gol krallığındaki oyuncuların gol dağılımları")
 plt.pie(kral["GOL"],labels=kral["OYUNCU"], autopct='%1.1f%%', explode=(kral["GOL"]==max(kral["GOL"]))*0.2, shadow=True)
